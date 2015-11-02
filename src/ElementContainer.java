f5835e9a5afe57e09a9798f3a64258da326d58e8
surajitb <surajitbarman57@yahoo.in>
2015-11-02 23:20:10 +0530

    Container classes are created for element and strength

---
+++ src/ElementContainer.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user2 on 11/2/15.
+ */
+public class ElementContainer {
+    private List<String> container;
+
+    public ElementContainer() {
+        container = new ArrayList<>();
+    }
+
+    public List<String> getContainer(){
+        return container;
+    }
+
+    public boolean isInserted(String data){
+        container.add(data);
+        return  true;
+    }
+
+    public boolean isPresentInContainer(String data){
+        return container.contains(data);
+    }
+}
---
+++ src/StrengthContainer.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user2 on 11/2/15.
+ */
+public class StrengthContainer {
+    private List<Integer> container;
+
+    public StrengthContainer() {
+        container = new ArrayList<>();
+    }
+
+    public List<Integer> getContainer(){
+        return container;
+    }
+
+    public boolean isInserted(int strength){
+        container.add(strength);
+        return true;
+    }
+
+    public boolean isPresentInContainer(String data){
+        return container.contains(data);
+    }
+}
