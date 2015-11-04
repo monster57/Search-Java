bcd96ec793669a89a490de451a2ec3b9424655f6
surajitb <surajitbarman57@yahoo.in>
2015-11-04 10:53:55 +0530

    Test is added for Element Container Class

---
+++ Tests/ElementContainerTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by user2 on 11/4/15.
+ */
+public class ElementContainerTest {
+
+    @Test
+    public void shouldAddElementToContainerWhenElementIsNotPresentInContainer(){
+        ElementContainer elementContainer = new ElementContainer();
+        String element = "hello";
+        
+        elementContainer.addElementToContainer(element);
+        assertTrue(elementContainer.getContainer().contains(element));
+    }
+}
