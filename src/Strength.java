15d1f1366d7597212c341df44f6a9f1347bfe897
surajitb <surajitbarman57@yahoo.in>
2015-11-04 11:11:35 +0530

    Strength class added

---
+++ src/Strength.java
@@ -0,0 +1,14 @@
+/**
+ * Created by user2 on 11/4/15.
+ */
+public class Strength {
+    private int value;
+
+    public Strength(int value) {
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
