9421af84b422d43678119561a8ccd0a2e7e48d65
surajitb <surajitbarman57@yahoo.in>
2015-11-04 11:24:47 +0530

    Element Class is Created

---
+++ src/Element.java
@@ -0,0 +1,14 @@
+/**
+ * Created by user2 on 11/4/15.
+ */
+public class Element {
+    private String value;
+
+    public Element(String value) {
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+}
